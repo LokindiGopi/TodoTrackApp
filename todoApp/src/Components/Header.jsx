092d207de7856d485469859bdd88161682c6b5cc
LokindiGopi <lokindigopi1503@gmail.com>
2024-08-01 17:41:54 -0500

    Updating the Header

--- todoApp/src/Components/Header.jsx
+++ todoApp/src/Components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="shadow-2x bg-gradient-to-r from-blue-900 font-serif h-50 pt-5 pb-3 text-2xl text-center text-slate-200">
-      My Todo List Track
+      My Todo List Track App
     </div>
   );
 }
