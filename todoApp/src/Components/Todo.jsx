fe3d8b44470df94ee97857d248aaffe57ff60296
LokindiGopi <lokindigopi1503@gmail.com>
2024-08-01 18:33:32 -0500

    Updated todo.jsx

--- todoApp/src/Components/Todo.jsx
+++ todoApp/src/Components/Todo.jsx
@@ -5,19 +5,28 @@ import Footer from "./Footer";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
-  
+
   console.log(todos);
   useEffect(() => {
-    setTodos(JSON.parse(localStorage.getItem("tasks")));
+    const storedTodos = JSON.parse(localStorage.getItem("tasks"));
+    if (storedTodos) {
+      setTodos(storedTodos);
+    }
   }, []);
-  const completedtodos = todos.filter((todo) => todo.status === 'done').length;
-  const inprogresstodos = todos.filter((todo) => todo.status === 'inprogress').length;
+  const completedtodos =
+    todos && todos.filter((todo) => todo.done === true).length;
+  const inprogresstodos =
+    todos && todos.filter((todo) => todo.status === "inprogress").length;
   const totaltodos = todos.length;
   return (
     <div>
       <Form todos={todos} setTodos={setTodos} />
       <TodoList todos={todos} setTodos={setTodos} />
-      <Footer completedtodos={completedtodos} totaltodos={totaltodos} inprogresstodos={inprogresstodos}/>
+      <Footer
+        completedtodos={completedtodos}
+        totaltodos={totaltodos}
+        inprogresstodos={inprogresstodos}
+      />
     </div>
   );
 }
